e331d2d5835d9ae50bdbfdc860939bdf7830876a
Bryan Rodriguez <bsteven1993@gmail.com>
2020-01-20 10:46:18 -0500

    stateful , se agrega metodos para sumar/restar/resetear

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
 
+  final fontH3 = TextStyle( fontSize: 30 );
+  final conteo = 10;
+
   @override
   Widget build( BuildContext context ){
     return  Scaffold(
@@ -13,10 +16,16 @@ class HomePage extends StatelessWidget{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Número de clicks:'),
-            Text('0')
+            Text('Número de clicks:', style: fontH3),
+            Text(' $conteo ', style: fontH3)
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon( Icons.add ),
+        onPressed: (){
+          print('Hola Mundo!');
+        },
       ),
     );
   }
